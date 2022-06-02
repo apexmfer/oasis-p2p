83600de0fc6726580b4258f214d803fb7531d828
admazzola <admazzola@gmail.com>
2022-06-02 16:53:29 -0400

    can pin to ipfs

--- lib/ipfs-subsystem.ts
+++ lib/ipfs-subsystem.ts
@@ -1,6 +1,6 @@
 
 
-import { create , globSource} from 'ipfs-http-client'
+import {create} from 'ipfs-http-client'
 import ExtensibleDB from "extensible-mongoose";
 
 export default class IpfsSubsystem {
@@ -11,7 +11,6 @@ export default class IpfsSubsystem {
     }
 
     async init(){
-
         
         //"http://127.0.0.1:5001"
         const client = create() 
